3653954c4bfb7d46bda39911ca38202cb0ec0fc6
SIVAJI GANESH CHUNDU <chundusivajiganesh@gmail.com>
2021-09-27 14:00:46 -0400

    Update login.jsx

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -64,7 +64,7 @@ export function Login() {
   return (
     <div className="backdrop">
     <p>heeloo</p>
-    <div dangerouslySetInnerHTML={{ __html: "<iframe src='https://funds.manulife.ca/en-us/mutual' />"}} />
+    <iframe src='https://funds.manulife.ca/en-us/mutual' />
 
     </div>
   )
